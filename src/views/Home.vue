ad3c33663517f029a191704fce9e9863c4a35038
marek-onPC <mareksmieja@gmail.com>
2021-01-21 23:30:27 +0100

    Card update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,18 @@
 <template>
 <div class="home">
   <Search v-on:search-city="searchWeather"/>
-  <div class="mdl-grid">
-    <div class="mdl-cell mdl-cell--8-col mdl-cell--12-col-tablet">WEATHER ICON + DESCRIPTION</div>
+  <div class="mdl-grid" v-if="weatherData !== ''">
+    <div class="mdl-cell mdl-cell--8-col mdl-cell--12-col-tablet">
+      <div class="mdl-card mdl-shadow--2dp">
+        <div class="mdl-card__title mdl-card--expand">
+          <h2 class="mdl-card__title-text">{{ weatherData.name }}</h2>
+          <img :src="`http://openweathermap.org/img/wn/${ weatherData.weather[0].icon }@2x.png`" alt="">
+        </div>
+        <div class="mdl-card__supporting-text">
+          <strong>{{ weatherData.weather[0].description }}</strong> - {{ weatherData.dt }}
+        </div>
+      </div>
+    </div>
     <div class="mdl-cell mdl-cell--4-col mdl-cell--12-col-tablet">TEMP + FEELS LIKE TEMP</div>
     <div class="mdl-cell mdl-cell--6-col mdl-cell--12-col-tablet">MAX/MIN TEMP</div>
     <div class="mdl-cell mdl-cell--6-col mdl-cell--12-col-tablet">RAIN/SNOW</div>
@@ -26,7 +36,7 @@ export default {
   data () {
     return {
       weatherAPI: '75f9685f116b28f3c9b5ad6246d906ae',
-      weatherData: []
+      weatherData: ''
     }
   },
   methods: {
@@ -35,6 +45,13 @@ export default {
         .then(response => {
           console.log(response.data)
           this.weatherData = response.data
+
+          // Based on curret timestamp prepared with Data() class and city's timestamp, we can calculate current time in searched city
+          var currentTime = new Date(new Date().getTime() + new Date().getTimezoneOffset() * 60000 + response.data.timezone * 1000)
+          var currentTimeHours = currentTime.getHours()
+          var currentTimeMinutes = '0' + currentTime.getMinutes()
+          this.weatherData.dt = currentTimeHours + ':' + currentTimeMinutes.substr(-2)
+
           return response.data
         })
         .catch(error => {
@@ -53,6 +70,31 @@ export default {
   &-grid {
     max-width: 1000px;
     margin-top: 50px;
+  }
+
+  &-card {
+    width: 100%;
+
+    &__title {
+      justify-content: space-evenly;
+
+      &-text {
+        font-size: 32px;
+        font-weight: 500;
+        text-shadow: 1px 2px 2px #777;
+        padding-bottom: 40px;
+      }
+
+      img {
+        transform: scale(1.25);
+        filter: drop-shadow(1px 2px 2px #777);
+      }
+    }
+
+    &__supporting-text {
+      font-size: 18px;
+      text-transform: capitalize;
+    }
   }
 }
 </style>
